777f0b9a1161243e53a93dae6a5d8de697a4b46a
111guga111 <guramtopchishvili@gmail.com>
2025-02-01 23:29:01 +0400

    first commit

---
+++ Day-00/Classwork/lesson-1 .py
@@ -0,0 +1,9 @@
+name ="Guga" #string (str)
+username = "Topchishvili" #string (str)
+age =16 #integer (int)
+height =160.5 #float 
+#booleon
+programming_is_fun=True
+knows_proggraming=False
+
+print (str(age) + ' ' + "year old" + ' ' + str(height) + " " + name + ' ' + username + ' ' + 'came home after school to learn programing') 
