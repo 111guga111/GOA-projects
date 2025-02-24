ce13596fbb4ffae4c50bc7cb2cc071ee1b19e615
111guga111 <guramtopchishvili@gmail.com>
2025-02-24 10:22:57 +0400

    fourth commit

---
+++ Day-03/Classwork .py
@@ -0,0 +1,62 @@
+# age=int(input("Age: "))
+
+# if False :
+#  print("hello")
+#  print("bye")
+#  print("wait") 
+# print("1")
+
+# x = 2 > 1
+
+# if age > 17:
+#  print("your old enough")
+# else:
+#  print("your not old enough")
+
+# if age==20:
+#  print("your right")
+ 
+# elif age==19:
+#  print("close enough")
+
+# elif age==18:
+#  print("not good but decent")
+
+# else:
+#  print("your cant get in")
+
+
+num=int(input('number '))
+if num % 2==0:
+ print("even")
+else:
+    print("odd")
+
+numm= int(input("number "))
+if numm < -1:
+ print(numm==-1)
+elif (numm==0):
+ print(numm==0)
+elif (numm>0):
+ print(numm==1)
+
+number=int(input("nummber "))
+number2=int(input("nummber2 "))
+if number>number2:
+ print (number)
+elif number<number2:
+ print(number2)
+
+number4=int(input("num "))
+if number4%5==0:
+    print(number4)
+else:
+    print(0)
+
+
+number5=int(input("year "))
+if number5>=2000:
+ print("your new age")
+ 
+elif number5<2000:
+ print("your old age")
---
+++ Day-03/Homework .py
@@ -0,0 +1,40 @@
+#Task 1
+age=int(input("age "))
+if 2<=age<6:
+    print("you need to go kindergarden")
+elif 0<age<2:
+    print("grow up little bro")
+elif age<=0:
+    print("your not even born yet")
+elif 6<=age<18:
+    print("you need to go to school")
+elif 18<=age:
+    print("you have to work")
+    
+#Task 2
+academy=input("where do you study? ")
+if academy == "Goa":
+    print("your a chad")
+elif academy=="GOA":
+    print("your a chad")
+elif academy=="goa":
+    print("your a chad")
+else:
+   print("join Goa and turn into chad")
+
+#Task 3
+score=int(input('what score did you get '))
+if score>=90:
+    print("you got an A")
+elif score>=75:
+    print("you got an B")
+elif score>=60:
+    print("you got an C")
+elif score>=50:
+     print("you got an D")
+elif score>=40:
+    print("you got an E")
+elif score>=30:
+    print("you got an F")
+else:
+    print("you got an F")
