5bcb2fe415ddd6d88f610c9202cea9d845d501bf
111guga111 <guramtopchishvili@gmail.com>
2025-02-26 20:49:21 +0400

    first commit

--- Day-00/Classwork/lesson-1 .py
+++ Day-00/Classwork/lesson-1 .py
@@ -2,8 +2,10 @@ name ="Guga" #string (str)
 username = "Topchishvili" #string (str)
 age =16 #integer (int)
 height =160.5 #float 
-#booleon
+
+#booleon (simple)
 programming_is_fun=True
 knows_proggraming=False
 
+#to create sentence
 print (str(age) + ' ' + "year old" + ' ' + str(height) + " " + name + ' ' + username + ' ' + 'came home after school to learn programing') 
