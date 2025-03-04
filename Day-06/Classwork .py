fe5a8b7054c0cd96d675e7a6b1e832c2aa193f7c
111guga111 <guramtopchishvili@gmail.com>
2025-03-04 20:49:18 +0400

    seventh commit

---
+++ Day-06/Classwork .py
@@ -0,0 +1,28 @@
+word=input("word: ")
+num=int(input("num: "))
+print((word)[num])
+
+
+for Y in range(100,1,-1):
+ print(Y)
+
+for I in range(1,100):
+  if I%2==1:
+    print(I)
+
+for O in range(250,500,10):
+  print(O)
+
+sum=input("word: ")
+print(sum[0]=="_")
+ 
+
+  
+number1=int(input("num: "))
+if number1 %2==0:
+ for U in range(10):
+   print("yes")
+else:
+  print("no")
+
+        
---
+++ Day-06/Homework .py
@@ -0,0 +1,4 @@
+num=int(input("num; "))
+for i in range(1,num):
+    if num%i==0:
+        print(i)
