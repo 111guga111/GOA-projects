3f01dc0208efb3bef86130e5a27f50d39738b26d
111guga111 <guramtopchishvili@gmail.com>
2025-03-04 21:05:12 +0400

    seventh commit

--- Day-06/Homework .py
+++ Day-06/Homework .py
@@ -1,4 +1,20 @@
+#1
 num=int(input("num; "))
 for i in range(1,num):
-    if num%i==0:
+    if num % i== 0:
         print(i)
+
+#2
+print("we use 'for' when we know how many iteration there is and we use while when we dont know how many iteration there is")
+
+#3
+print("i have looked into the extra video and it contained information about while loops")
+
+#4
+print("(true and false) false")
+print("(true and true) true")
+print("(false or false) false")
+
+#5
+print("'while' helps us create loop so we could stop the user from continuing if they give invalid answear or we might want to keep repeating come until specific thing changes, we could also use 'while not', this mean while it doesnt instead of while it does")
+print("'for' helps us create a range for variable that helps us print seperatly numbers or letter on number or word, it also helps us create a code that we can do whatever we want if the first or any letter in the word is whatever we decide")
