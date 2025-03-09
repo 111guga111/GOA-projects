ab4f6a15080eb6bd5a128ffae67b2e0694b8d3ff
111guga111 <guramtopchishvili@gmail.com>
2025-03-09 19:14:59 +0400

    eighth commit

--- Day-07/Homework/homework (hard) .py
+++ Day-07/Homework/homework (hard) .py
@@ -6,10 +6,10 @@ while num1!=num2 and num1!=2:
     if len(str(num1))!=4 and len(str(num1))!=1:
         num1=int(input("it has to be one or four digit: "))
     
-    elif int(str(num1)[0])!=1 and num1!=2 and len(str(num1))!=1:
+    elif int(str(num1)[0])!=1 and len(str(num1))!=1:
         num1=int(input("number starts with 1 : "))
    
-    elif int(str(num1)[0])==1 and num1!=2 or len(str(num1))==1:
+    elif int(str(num1)[0])==1 or len(str(num1))==1:
         num1=int(input("wrong try again: "))
 
 print("CORRECT!,YOU WON!")
