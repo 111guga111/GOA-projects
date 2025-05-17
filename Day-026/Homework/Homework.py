8934f4fa145047f382947ee0846bfb0ed64224fb
111guga111 <guramtopchishvili@gmail.com>
2025-05-17 10:46:58 +0400

    commit

--- Day-026/Homework/Homework.py
+++ Day-026/Homework/Homework.py
@@ -1,8 +1,63 @@
-#1 https://www.codewars.com/kata/55a996e0e8520afab9000055
-def cookie(x):
-    if type(x)==str:
-        return "Who ate the last cookie? It was Zach!"
-    elif type(x)==float or type(x)==int:
-        return "Who ate the last cookie? It was Monica!"
-    else:
-        return "Who ate the last cookie? It was the dog!"
+#1
+def data(a):
+    if type(a)==str:
+        return "Literature"
+    elif type(a)==bool:
+        return "Science"
+    return "Math"
+print(data(2))
+
+#2
+def datas(a):
+    lst = []
+    for i in a:
+        lst.append(type(i))
+
+    strings = lst.count(str)
+    integers = lst.count(int)
+    booleons = lst.count(bool)
+    floats = lst.count(float)
+    most = max(strings, integers, booleons, floats)
+    
+    if most == strings:
+        return str
+    elif most == integers:
+        return int
+    elif most == booleons:
+        return bool
+    elif most == floats:
+        return float
+print (datas(["guga","nika",10,12,40,True,False,True]))
+
+#3
+def dataclean(a):
+    lst = []
+    for i in a:
+        lst.append(type(i))
+
+    strings = lst.count(str)
+    integers = lst.count(int)
+    booleons = lst.count(bool)
+    floats = lst.count(float)
+    most = max(strings, integers, booleons, floats)
+
+    if most == strings:
+        datatype=str
+    elif most == integers:
+        datatype=int
+    elif most == booleons:
+        datatype=bool
+    elif most == floats:
+        datatype=float
+    
+    result=[]
+    for i in a:
+        if type(i)==datatype:
+            result.append(i)
+    return result
+print (dataclean(["world","dog","water","movie",10,12,40,True,False,True]))
+ 
+ #4
+def equal(a):
+    return type(a)
+print(equal(5+6.5))
---
+++ Day-027/Homework/homework.py
@@ -0,0 +1,75 @@
+
+#1 function
+def time(d,m,y):
+    month = ["january", "february", "march", "april", "may", "june","july", "august", "september", "october", "november", "december"]
+    if str(m).isalpha():
+        m=m.lower()
+    for i in month:
+        if m==i:
+            m=month.index(m)+1
+    if m<10:
+        m="0"+str(m)
+    return f"{d}/{m}/{y}"
+print(time(14,"APRil",2032))
+
+#1
+month = ["january", "february", "march", "april", "may", "june","july", "august", "september", "october", "november", "december"]
+d=input("day: ")
+m=input("month: ")
+y=input("year: ")
+if m.isalpha():
+    m=m.lower()
+for i in month:
+    if m==i:
+        m=str(month.index(m)+1)
+if len(m)<2:
+    m="0"+m
+print(f"{d}/{m}/{y}")
+
+
+
+#2 function
+def password(a):
+    if len(str(a))<8:
+        return "Length should be 8 or more then 8"
+    return "Password accepted"
+print(password("gugugugug12123"))
+
+#2 
+password=input("Password: ")
+if len(password)<8:
+    print("Length should be 8 or more then 8")
+elif len(password)>=8:
+    print("Password accepted")
+
+
+
+#3 function
+def biggest(a,b,c):
+    nums=[a,b,c]
+    return max(nums)
+print(biggest(1,8,10))
+
+#3
+num=int(input("number: "))
+num1=int(input("number: "))
+num2=int(input("number: "))
+nums=[num,num1,num2]
+print(max(nums))
+
+
+
+#4 function
+def biggest(a,b,c,d,e):
+    nums=[a,b,c,d,e]
+    return sum(nums)
+print(biggest(1,8,10,20,80))
+
+#4
+num=int(input("number: "))
+num1=int(input("number: "))
+num2=int(input("number: "))
+num3=int(input("number: "))
+num4=int(input("number: "))
+nums=[num,num1,num2,num3,num4]
+print(sum(nums))
